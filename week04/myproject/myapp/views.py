7c88b9f5aa5c8e0a7a1ca80fbdf52878840f29bf
Wasit Limprasert <wasit7@gmail.com>
2022-02-03 16:02:17 +0700

    create view

--- week04/myproject/myapp/views.py
+++ week04/myproject/myapp/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from myapp.models import Student
+from django.views.generic.edit import CreateView
+from django.urls import reverse
 
-# Create your views here.
+class StudentCreateView(CreateView):
+    model = Student
+    template_name = 'student_create.html'
+    fields = ['name','nick_name','email','phone_number']
+    def get_success_url(self):
+        return reverse('student_create')
+        # kwargs={'pk': self.object.pk}
--- week04/myproject/myproject/urls.py
+++ week04/myproject/myproject/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from myapp.views import StudentCreateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('register/', StudentCreateView.as_view(),name='student_create'),
 ]
