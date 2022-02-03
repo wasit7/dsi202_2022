29275945f82015ecbf36bbdd7019867d43f5f7c8
Wasit Limprasert <wasit7@gmail.com>
2022-02-03 15:26:08 +0700

    update model and admin page

--- week04/myproject/myapp/models.py
+++ week04/myproject/myapp/models.py
@@ -1,7 +1,9 @@
 from django.db import models
 
-class Student(models.Models):
+class Student(models.Model):
     name = models.CharField(max_length=100)
     nick_name = models.CharField(max_length=50)
-    email = models.Email()
+    email = models.EmailField()
     phone_number = models.CharField(max_length=11)
+    def __str__(self):
+        return "%s"%(self.name)
