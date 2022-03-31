68dec3d4e5768fc3b169694d3b5405ddf4453cac
Wasit Limprasert <wasit7@gmail.com>
2022-03-31 15:33:35 +0700

    during class 11

--- week11/myproject/myproject/urls.py
+++ week11/myproject/myproject/urls.py
@@ -17,8 +17,11 @@ from django.contrib import admin
 from django.urls import path
 from myapp import views
 
+from django.conf import settings
+from django.conf.urls.static import static
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',views.home, name='home'),
     path('profile/',views.profile, name='profile'),
-]
+]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
